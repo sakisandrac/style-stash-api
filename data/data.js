40b23768c37e0cabb55c7676b22c1425fd54ef70
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-17 19:29:04 -0400

    Refactor data structure to relational data

--- data/data.js
+++ data/data.js
@@ -1,92 +1,86 @@
 module.exports = [
   {
-    closet: {
-      pants: [
-        {
-          id: "pants-ex-1",
-          image: "./pants.JPG",
-          category: "pants",
-          notes: ""
-        }
-      ],
-      tops: [
-        {
-          id: "tops-ex-1",
-          image: "./top.JPG",
-          category: "tops",
-          notes: ""
-        }
-      ],
-      skirts: [
-        {
-          id: "skirts-ex-1",
-          image: "./skirt.JPG",
-          category: "skirts",
-          notes: ""
-        }
-      ],
-      onePieces: [
-        {
-          id: "onepiece-ex-1",
-          image: "./romper.JPG",
-          category: "onePieces",
-          notes: ""
-        }
-      ],
-      shoes: [
-        {
-          id: "shoes-ex-1",
-          image: "./shoes.JPG",
-          category: "shoes",
-          notes: ""
-        }
-      ],
-      bags: [
-        {
-          id: "bag-ex-1",
-          image: "./bag.JPG",
-          category: "bags",
-          notes: ""
-        }
-      ],
-      accessories: [
-        {
-          id: "accessories-ex-1",
-          image: "./necklace.jpg",
-          category: "accessories",
-          notes: ""
-        }
-      ],
-      outerwear: [
-        {
-          id: "outerwear-ex-1",
-          image: "./jacket.JPG",
-          category: "outerwear",
-          notes: ""
-        }
-      ],
-      misc: [
-        {
-          id: "misc-ex-1",
-          image: "./hat.JPG",
-          category: "misc",
-          notes: ""
-        }
-      ],
-    },
-    outfits: [
+    pieces: [
       {
-        id: "",
-        pieces: [
-          {
-            id: "",
-            image: "",
-            notes: "",
-            category: ""
-          }
-        ],
+      id: "PIE-pants-ex-1",
+      image: "./pants.JPG",
+      categoryID: 'CAT-pants',
+      notes: ""
+      }, 
+      {
+        id: "PIE-tops-ex-1",
+        image: "./top.JPG",
+        categoryID: "CAT-tops",
+        notes: ""
+      },
+      {
+        id: "PIE-skirts-ex-1",
+        image: "./skirt.JPG",
+        categoryID: "CAT-skirts",
+        notes: ""
+      },
+      {
+        id: "PIE-onepiece-ex-1",
+        image: "./romper.JPG",
+        categoryID: "CAT-onePieces",
+        notes: ""
+      },
+      {
+        id: "PIE-shoes-ex-1",
+        image: "./shoes.JPG",
+        categoryID: "CAT-shoes",
+        notes: ""
+      },
+      {
+        id: "PIE-bag-ex-1",
+        image: "./bag.JPG",
+        category: "CAT-bags",
+        notes: ""
+      },
+      {
+        id: "PIE-accessories-ex-1",
+        image: "./necklace.jpg",
+        category: "CAT-accessories",
+        notes: ""
+      },
+      {
+        id: "PIE-outerwear-ex-1",
+        image: "./jacket.JPG",
+        category: "CAT-outerwear",
+        notes: ""
+      },
+      {
+        id: "PIE-misc-ex-1",
+        image: "./hat.JPG",
+        category: "CAT-miscellaneous",
         notes: ""
       }
+    ],
+    categories: [
+      {id: 'CAT-pants', name: 'Pants'},
+      {id: 'CAT-tops', name: 'Tops'},
+      {id: 'CAT-skirts', name: 'Skirts'},
+      {id: 'CAT-onePieces', name: 'One Pieces'},
+      {id: 'CAT-shoes', name: 'Shoes'},
+      {id: 'CAT-bags', name: 'Bags'},
+      {id: 'CAT-accessories', name: 'Accessories'},
+      {id: 'CAT-outerwear', name: 'Outer Wear'},
+      {id: 'CAT-miscellaneous', name: 'Miscellaneous'}
+    ],
+    outfitToPieces: [
+      {id: "OTP-otp-ex-1", outfitID: "OUT-outfit-ex-1", pieceID: 'PIE-pants-ex-1'},
+      {id: "OTP-otp-ex-2", outfitID: "OUT-outfit-ex-1", pieceID: 'PIE-tops-ex-1'},
+      {id: "OTP-otp-ex-3", outfitID: "OUT-outfit-ex-1", pieceID: 'PIE-accessories-ex-1'},
+      {id: "OTP-otp-ex-4", outfitID: "OUT-outfit-ex-1", pieceID: 'PIE-shoes-ex-1'},
+      {id: "OTP-otp-ex-5", outfitID: "OUT-outfit-ex-1", pieceID: 'PIE-bags-ex-1'},
+      {id: "OTP-otp-ex-6", outfitID: "OUT-outfit-ex-2", pieceID: 'PIE-onepiece-ex-1'},
+      {id: "OTP-otp-ex-7", outfitID: "OUT-outfit-ex-2", pieceID: 'PIE-outerwear-ex-1'},
+      {id: "OTP-otp-ex-8", outfitID: "OUT-outfit-ex-2", pieceID: 'PIE-shoes-ex-1'},
+      {id: "OTP-otp-ex-9", outfitID: "OUT-outfit-ex-2", pieceID: 'PIE-bags-ex-1'},
+    ],
+    outfits: [
+      {id: "outfit-ex-1", fullOutfitImage: "./outfit.JPG"},
+      {id: "outfit-ex-2", fullOutfitImage: "./outfit2.png"}
     ]
   }
 ]
